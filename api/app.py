483f1727d1808c3c3ce5a8bdb254734c61ab822d
yevhenii.ravliuk <yevhenii.ravliuk@jetsoftpro.com>
2023-04-28 07:24:04 +0300

    Fix title of subject v2

--- api/app.py
+++ api/app.py
@@ -1,26 +1,13 @@
 import uvicorn
-from fastapi import FastAPI, Request, Body
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import select, update
 from starlette.staticfiles import StaticFiles
 
-from sqlmodel import SQLModel, create_engine, Session
-
 from services import get_subjects, read_questions
-
-from models import QuizProgress, Base
-from db import engine
-from depends import Session
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# quiz_progress = QuizProgress(user_id=1, question_id=1, answer_index=2)
-# with Session(engine) as session:
-#     session.add(quiz_progress)
-#     session.commit()
 
 
 @app.get('/')
@@ -39,39 +26,8 @@ def pdr(request: Request):
 def subject(request: Request, pk: int | float):
     subject_name = get_subjects().get(pk)
     questions = read_questions(subject_name)
-    return templates.TemplateResponse("subject.html", {"request": request, 'questions': questions, 'title': subject_name})
-
-
-@app.post("/save_progress")
-async def save_progress(body: dict = Body(...)):
-    print('starting')
-
-    user_id = body['user_id']
-    question_id = body['question_id']
-    answer_index = body['answer_index']
-    subject = body['subject']
-
-    print(body)
-
-    existing_answer = await Session.select(select(QuizProgress).where(QuizProgress.user_id == user_id).where(
-        QuizProgress.question_id == question_id))
-
-    obj = existing_answer.fetchone()
-
-    if existing_answer:
-        existing_answer.answer_index = answer_index
-        Session.add(obj)
-        Session.commit()
-        Session.refresh(obj)
-
-    else:
-        new_answer = QuizProgress(user_id=user_id, question_id=question_id, answer_index=answer_index,
-                                  subject=subject)
-        Session.add(new_answer)
-
-    Session.commit()
-
-    return {"status": "success"}
+    return templates.TemplateResponse("subject.html",
+                                      {"request": request, 'questions': questions, 'title': subject_name})
 
 
 if __name__ == "__main__":
