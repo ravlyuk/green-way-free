91533c8773474fd7043856a59acbd8293ec79037
yevhenii.ravliuk <yevhenii.ravliuk@jetsoftpro.com>
2023-12-30 13:06:37 +0200

    Fix questions

--- api/test.py
+++ api/test.py
@@ -1,4 +1,15 @@
-questions = ",".join([str(num) for num in list(range(0, 3500))])
+import requests
+from lxml.html import fromstring
 
-payload = f'method=getJsonQuestions&data=%7B%22questionIds%22:%22{questions}%22,%22locale%22:%22uk%22%7D'
-print(payload)
+url = "https://www.forexfactory.com/calendar?day=today"
+
+import httpx
+resp = httpx.get("https://example.com").text
+
+# resp = requests.get(url).text
+
+page = fromstring(resp)
+
+elems = page.xpath("//tr")
+
+print(resp)
